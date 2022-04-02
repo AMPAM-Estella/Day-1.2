62b9ab303a82df9911d8c4ab6ca26b78f1b7d954
Estella Abo <102621398+AMPAM-Estella@users.noreply.github.com>
2022-04-02 17:04:04 +0000

    File name

--- main.py
+++
@@ -1,5 +0,0 @@
-# Write your code below this line 👇
-print("Day - 1 String Manilulation")
-print('String Concatenation is done with the "+" sign')
-print('e.g. print("Hello" + "world")')
-print("New lines can be created with a backlash and n.")
